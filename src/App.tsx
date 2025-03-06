5b619b056a1dd092fb9fa3491b65d8256728a537
Priyanshu <priyanshusingh.0601@gmai.com>
2025-03-06 22:48:26 +0530

    add home page

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
     <BrowserRouter>
       <ToastContainer autoClose={3000}/>
       <Routes>
+        <Route path="/" element={<Signup />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/dashboard" element={<Dashboard />} />
